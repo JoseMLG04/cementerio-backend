6fd2b42ae45d463d757a4ec39ba56c42c5225bbc
WilliamCuc <wcuc96@gmail.com>
2025-10-11 20:20:29 -0600

    Cambios en conexion a supabase

--- config/supabase.js
+++ config/supabase.js
@@ -1,6 +1,13 @@
 import postgres from "postgres";
 
 const connectionString = process.env.SUPABASE_URL;
-const sql = postgres(connectionString);
+const sql = postgres(connectionString, {
+  socket: {
+    family: 4,
+  },
+  max: 10,
+  idle_timeout: 20,
+  connect_timeout: 10,
+});
 
 export default sql;
