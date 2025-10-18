75b5f204aaf44ecb23d56cad139dd862879a66cf
Jose Lopez <114269446+JoseMLG04@users.noreply.github.com>
2025-10-17 21:22:35 -0600

    Add new routes for searching and retrieving panteones

--- routes/panteonesRoutes.js
+++ routes/panteonesRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import {
   obtenerPanteones,
   obtenerTodosPanteones,
+  buscarPanteones,           
+  obtenerPanteonPorCodigo,   
   crearPanteon,
   editarPanteon,
   eliminarPanteon,
@@ -9,8 +11,11 @@ import {
 
 const router = express.Router();
 
+router.get("/buscar", buscarPanteones);
+router.get("/todos", obtenerTodosPanteones);         
+router.get("/codigo/:codigo", obtenerPanteonPorCodigo);
+
 router.get("/", obtenerPanteones);
-router.get("/todos", obtenerTodosPanteones);
 router.post("/", crearPanteon);
 router.put("/:id", editarPanteon);
 router.delete("/:id", eliminarPanteon);
